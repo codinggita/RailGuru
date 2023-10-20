9fb3051c3f50d1c36088f99396d6c21e7b08850f
“prokrishpatel” <pro.krishpatel@gmail.com>
2023-10-20 22:10:16 +0530

    home

--- Working/script.js
+++ Working/script.js
@@ -30,15 +30,23 @@ barElement.addEventListener("click",function(){
     }
 })
 
-// cnhange text animation 
-
-var changeText = document.getElementById('changetext');
-const textOptions = ["Check PNR status", "Check Seat Availability", "Find Train", "Many More..."];
-let currentTextIndex = 0;
-function changeT(){
-    changeText.innerText = textOptions[currentTextIndex++];
-    if(currentTextIndex===4){
-        currentTextIndex = 0;
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-}
-setInterval(changeT,3000);
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
