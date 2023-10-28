8ad774e0ab9ca01154cba9e094f1d182025f796c
“prokrishpatel” <pro.krishpatel@gmail.com>
2023-10-28 15:47:04 +0530

    Where to go

---
+++ Working/wherto.js
@@ -0,0 +1,4 @@
+const fromStation = document.getElementById('fromstation');
+const toStation = document.getElementById('tostation');
+const date = document.getElementById('date');
+const searchButton = document.getElementById('find-train');
