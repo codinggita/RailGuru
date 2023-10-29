5408f5fe4869f67b3d16d95278280c775596c390
“prokrishpatel” <pro.krishpatel@gmail.com>
2023-10-30 02:09:03 +0530

    final

--- Working/script.js
+++ Working/script.js
@@ -56,3 +56,20 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   }
 document.querySelector(".tablinks").click()
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds (2000 milliseconds)
+}
+
+showSlides();
